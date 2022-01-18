12e4bea44d6a2139cbaa968720b6d3d65824b7d2
reinholz36 <dreinholz28@gmail.com>
2022-01-18 15:15:20 -0500

    updated player function in player.js

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,10 +2,7 @@ const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
-
 const Player = require('../lib/Player');
-const { expect } = require('@jest/globals');
 
 
 test('creates a player object', () => {
@@ -18,4 +15,24 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
